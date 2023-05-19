75a12330ffb90b35834dc3d0cd5228471e279274
Binyang Han <hanbinyangwh@gmail.com>
2023-05-19 14:19:42 +0800

    new branch

--- BITengine/spiders/bit.py
+++ BITengine/spiders/bit.py
@@ -14,11 +14,17 @@ class BITSpider(scrapy.Spider):
         'bit.edu.cn',
     ]
 
-    subdir='htmlSource'
-    if not os.path.exists(subdir):
-        os.mkdir(subdir)
+    
 
     def start_requests(self):
+        subdir='htmlSource'
+        os.rmdir(subdir)
+        os.mkdir(subdir)
+        with open('file_url.txt','w') as g:
+                    g.write('')
+        with open('error_url.txt', 'w') as h:
+                h.write('')
+
         for u in self.start_urls:
             yield scrapy.Request(u,callback=self.parse,errback=self.errorback)
 
@@ -33,30 +39,18 @@ class BITSpider(scrapy.Spider):
                 # h.write(failure.value.response)
         
     def parse(self, response):
-        # if url end with docx, doc, xls, zip, or pdf, then do something else.
-        # if is video or image, don't download.
-        if response.url.split('.')[-1] in {'pdf','doc','docx','ppt','pptx','xls','xlsx','zip'}:
-            pass
-        else:
-            filename = response.url.replace("/",'_')[7:]
-            if filename[0]=='_':
-                filename=filename[1:]
-            if filename[-1]=='_':
-                filename=filename[:-1]+'.html'
-            filepath=os.path.join(self.subdir,filename)
-            #
-            name_text = response.css("meta").xpath("@name").getall()
-            content_text = response.css("meta").xpath("@content").getall()
-            html_text = response.css("a").xpath("@href").getall()
-            title = response.xpath("//title/text()").get() 
-            #sel = scrapy.Selector(text=response.body)
-            #title_text = sel.xpath('//title/text()').get()
-            with open(filepath, 'wb') as f:
-                f.writelines(title+'\n')
-                f.writelines(name+'\n'  for name in name_text)
-                f.writelines(content+'\n' for content in content_text)
-                f.writelines(html+'\n' for html in html_text)
+        # assume given valid url. not file or picture or something.
 
+        # extract visiable text in html, then save to a file.
+
+            # filename = response.url.replace("/",'_')[7:]
+            # if filename[0]=='_':
+            #     filename=filename[1:]
+            # if filename[-1]=='_':
+            #     filename=filename[:-1]+'.html'
+            # filepath=os.path.join(self.subdir,filename)
+            #
+      
 
         yield {     # return some results
             'url': response.url,
@@ -64,11 +58,10 @@ class BITSpider(scrapy.Spider):
             # 'title': response.css('title::text')
         }
 
+        #find suburls to follow
+
         suburls = response.css('a::attr(href)').getall()  # find all sub urls
         for suburl in suburls:
-        # if url end with docx, doc, xls, zip, or pdf, then do something else.
-        # if is video or image, don't download.
-        # why not only download htm, html and php?
             if suburl.split('.')[-1] in {'pdf','doc','docx','ppt','pptx','xls','xlsx','zip','rar','jpg','jpeg','gif','png','svg','mp4','mov','avi','flv','mkv','mp3','wma','exe','msi','pkg','iso','dmg'}:
                 with open('file_url.txt','a') as g:
                     g.write(suburl+'\n')
@@ -79,5 +72,11 @@ class BITSpider(scrapy.Spider):
 
             if 'mailto' in suburl: #do not crawl mailto urls.
                 continue
-
-            yield response.follow(suburl, callback=self.parse,errback=self.errorback)
+            # only follow .htm, .html and .php links.
+            if '.htm' in suburl:
+                yield response.follow(suburl, callback=self.parse,errback=self.errorback)
+            
+            if '.php' in suburl:
+                yield response.follow(suburl, callback=self.parse,errback=self.errorback)
+            
+# i created a new branch.
